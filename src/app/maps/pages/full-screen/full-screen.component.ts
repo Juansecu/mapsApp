4ea5f4bf39f29b2fc6570f36dbdd115513100b61
Juansecu <juansecoder@hotmail.com>
2021-08-01 13:38:26 -0500

    Token configuration from Mapbox was moved to AppComponent

--- src/app/maps/pages/full-screen/full-screen.component.ts
+++ src/app/maps/pages/full-screen/full-screen.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
-import { environment } from 'src/environments/environment';
-
 @Component({
   selector: 'app-full-screen',
   templateUrl: './full-screen.component.html',
@@ -19,7 +17,6 @@ export class FullScreenComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    (mapboxgl as any).accessToken = environment.mapboxToken;
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/mapbox/streets-v11',
