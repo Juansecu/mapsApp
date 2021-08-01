18364b24a3f5982f2e49f6dd3796302e97fb01b1
Juansecu <juansecoder@hotmail.com>
2021-08-01 15:53:15 -0500

    Template for ZoomRangeComponent from MapsModule was setted

--- src/app/maps/pages/zoom-range/zoom-range.component.ts
+++ src/app/maps/pages/zoom-range/zoom-range.component.ts
@@ -1,16 +1,75 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
-  ]
+    `
+      .map-container {
+        width: 100%;
+        height: 100%;
+      }
+
+      .zoom-info {
+        position: fixed;
+        left: 3rem;
+        bottom: 3rem;
+        border-radius: 5px;
+        width: 50%;
+        padding: 0.7rem;
+        background-color: white;
+        z-index: 999;
+      }
+    `,
+  ],
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('mapContainer') mapContainer!: ElementRef;
 
-  constructor() { }
+  map!: mapboxgl.Map;
 
-  ngOnInit(): void {
+  zoomLevel = 10;
+  mapPosition: [number, number] = [-75.921029433568, 45.28719674822362];
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    this.map = new mapboxgl.Map({
+      center: this.mapPosition,
+      container: this.mapContainer.nativeElement,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      zoom: this.zoomLevel,
+    });
+
+    this.map.on('move', (event) => {
+      const { lng, lat } = event.target.getCenter();
+      this.mapPosition = [lng, lat];
+    });
+    this.map.on('zoom', () => (this.zoomLevel = this.map.getZoom()));
+    this.map.on('zoomend', () => {
+      if (this.map.getZoom() > 18) this.map.zoomTo(18);
+    });
   }
 
+  ngOnDestroy(): void {
+    this.map.off('move', () => {});
+    this.map.off('zoom', () => {});
+    this.map.off('zoomend', () => {});
+  }
+
+  changeZoom(zoomIn: boolean): void {
+    zoomIn ? this.map.zoomIn() : this.map.zoomOut();
+    this.zoomLevel = this.map.getZoom();
+  }
+
+  onZoomChange(inputValue: any): void {
+    this.map.zoomTo(inputValue * 1);
+  }
 }
