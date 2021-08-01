e5f7036497db2a63c798de21b6a63e5f3917c4fa
Juansecu <juansecoder@hotmail.com>
2021-08-01 10:51:22 -0500

    Routes for MapsModule were setted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'maps',
+    loadChildren: () => import('./maps/maps.module').then((m) => m.MapsModule),
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: 'maps',
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/maps/maps-routing.module.ts
+++ src/app/maps/maps-routing.module.ts
@@ -1,10 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { FullScreenComponent } from './pages/full-screen/full-screen.component';
+import { MarkersComponent } from './pages/markers/markers.component';
+import { PropertiesComponent } from './pages/properties/properties.component';
+import { ZoomRangeComponent } from './pages/zoom-range/zoom-range.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'fullscreen',
+        component: FullScreenComponent,
+      },
+      {
+        path: 'markers',
+        component: MarkersComponent,
+      },
+      {
+        path: 'properties',
+        component: PropertiesComponent,
+      },
+      {
+        path: 'zoom-range',
+        component: ZoomRangeComponent,
+      },
+      {
+        path: '**',
+        pathMatch: 'full',
+        redirectTo: 'fullscreen',
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class MapsRoutingModule { }
+export class MapsRoutingModule {}
