32e33edfb88bf1e7e88eab90c12d47d4fbbde0aa
Juansecu <juansecoder@hotmail.com>
2021-08-01 10:41:36 -0500

    Initial structure was setted

---
+++ src/app/maps/maps.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MapsRoutingModule } from './maps-routing.module';
+
+import { MiniMapComponent } from './components/mini-map/mini-map.component';
+import { FullScreenComponent } from './pages/full-screen/full-screen.component';
+import { MarkersComponent } from './pages/markers/markers.component';
+import { PropertiesComponent } from './pages/properties/properties.component';
+import { ZoomRangeComponent } from './pages/zoom-range/zoom-range.component';
+
+@NgModule({
+  declarations: [
+    FullScreenComponent,
+    MarkersComponent,
+    MiniMapComponent,
+    PropertiesComponent,
+    ZoomRangeComponent,
+  ],
+  imports: [CommonModule, MapsRoutingModule],
+})
+export class MapsModule {}
