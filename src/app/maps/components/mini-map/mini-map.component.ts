72d250f8a31b6df3e75a1c050a27d883108badb1
Juansecu <juansecoder@hotmail.com>
2021-08-01 21:31:10 -0500

    Template for MiniMapComponent from MapsModule was setted

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,16 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
 
 @Component({
   selector: 'app-mini-map',
   templateUrl: './mini-map.component.html',
   styles: [
-  ]
+    `
+      div {
+        width: 100%;
+        height: 12rem;
+      }
+    `,
+  ],
 })
-export class MiniMapComponent implements OnInit {
+export class MiniMapComponent implements AfterViewInit {
+  @Input() lngLat!: [number, number];
+  @ViewChild('mapContainer') mapContainer!: ElementRef;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    const map = new Map({
+      center: this.lngLat,
+      container: this.mapContainer.nativeElement,
+      interactive: false,
+      style: 'mapbox://styles/mapbox/streets-v11',
+      zoom: 15,
+    });
+
+    new Marker().setLngLat(this.lngLat).addTo(map);
   }
-
 }
