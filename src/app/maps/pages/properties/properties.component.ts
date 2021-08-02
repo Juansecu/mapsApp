2a327a8ac6ec64d812f035bcdd15ff45b08a02e5
Juansecu <juansecoder@hotmail.com>
2021-08-01 21:32:01 -0500

    Template for PropertiesComponent from MapsModule was setted

--- src/app/maps/pages/properties/properties.component.ts
+++ src/app/maps/pages/properties/properties.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { IProperty } from '../../typings/Property';
+
+import { PropertiesService } from '../../services/properties.service';
 
 @Component({
   selector: 'app-properties',
   templateUrl: './properties.component.html',
-  styles: [
-  ]
+  styles: [],
 })
-export class PropertiesComponent implements OnInit {
+export class PropertiesComponent {
+  properties: IProperty[] = this._propertiesService.properties;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor(private readonly _propertiesService: PropertiesService) {}
 }
